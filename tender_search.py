f07b5ae5f8c73b684b059b3c652a5e65a5357c1c
Nityesh Agarwal <nityeshagarwal@gmail.com>
2018-08-06 23:26:47 +0530

    tender_search.py: Added SEARCH_QUERIES
    Created a global list of search queries for which the website is to be
    queried. It is `['pp bag', 'fibc bag', 'jumbo bag', 'leno bag']`.

--- tender_search.py
+++ tender_search.py
@@ -1,15 +1,21 @@
 import mechanicalsoup as msoup
 import pandas as pd
 
+SEARCH_QUERIES = ['pp bag', 'fibc bag', 'jumbo bag', 'leno bag']
+
 if __name__ == '__main__':
+
     browser = msoup.StatefulBrowser()
-    browser.open("https://eprocure.gov.in/cppp/tendersearch")
-    browser.select_form()
-    browser.select_form('form[id="tendersearch-form"]')
-    browser["s_keyword"] = "pp bag"
-    response = browser.submit_selected()
-    result_page = browser.get_current_page()
-    tender_table = result_page.find('table', attrs={'id': 'table'})
-    tender_rows = tender_table.find_all('tr')
-    tender_df = pd.read_html(str(tender_table))
-    print(tender_df)
+
+    for query in SEARCH_QUERIES:
+        browser.open("https://eprocure.gov.in/cppp/tendersearch")
+        browser.select_form()
+        browser.select_form('form[id="tendersearch-form"]')
+        browser["s_keyword"] = query
+        response = browser.submit_selected()
+        result_page = browser.get_current_page()
+        tender_table = result_page.find('table', attrs={'id': 'table'})
+        tender_rows = tender_table.find_all('tr')
+        tender_df = pd.read_html(str(tender_table))
+        print('*'*10, query, '*'*10)
+        print(tender_df)

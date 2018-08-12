572b63e5ffa021d33f1e8e30ed31838e198aaf77
Nityesh Agarwal <nityeshagarwal@gmail.com>
2018-08-12 17:24:35 +0530

    tender_search.py: preprocess() & update_results()
    Added 2 new functions preprocess() and update_results():
    
    preprocess(): Preprocesses the given DataFrame object by setting
    appropriate row and column headers.
    update_results(): Stores and pdates the database related to the
    search query

--- tender_search.py
+++ tender_search.py
@@ -1,7 +1,47 @@
 import mechanicalsoup as msoup
 import pandas as pd
+import os.path
 
 SEARCH_QUERIES = ['pp bag', 'fibc bag', 'jumbo bag', 'leno bag']
+
+def preprocess(subject_df):
+    '''
+    Preprocesses the given DataFrame object by setting appropriate row
+    and column headers
+
+    :param subject_df: pandas DataFrame object
+    '''
+
+    # set column headers
+    headers = subject_df.iloc[0]
+    subject_df = subject_df.iloc[1:].copy(deep=True)
+    subject_df.rename(columns=headers, inplace=True)
+    
+    subject_df.set_index('Sl.No.', inplace=True)
+
+    return subject_df
+
+def update_results(query, tender_df):
+    '''
+    Updates the datebase related to the query with the entries in tender_df
+
+    :param query: The search query entered on the website
+    :param tender_df: A pandas DataFrame that stores the results returned by 
+                      the website 
+    '''
+
+    filename = query.replace(' ', '_') + '_tenders.csv'
+
+    if os.path.isfile('./'+filename):
+        database_df = pd.read_csv(filename, index_col='Sl.No.')
+        if database_df.equals(tender_df):
+            print("No new entries found")
+        else:
+            print("Updating database")
+            database_df = tender_df.copy(deep=True)
+            database_df.to_csv(filename)
+    else:
+        tender_df.to_csv(filename)
 
 if __name__ == '__main__':
 
@@ -13,9 +53,16 @@ if __name__ == '__main__':
         browser.select_form('form[id="tendersearch-form"]')
         browser["s_keyword"] = query
         response = browser.submit_selected()
+
+        # get results
         result_page = browser.get_current_page()
         tender_table = result_page.find('table', attrs={'id': 'table'})
-        tender_rows = tender_table.find_all('tr')
         tender_df = pd.read_html(str(tender_table))
+
+        tender_df_preprocessed = preprocess(tender_df[0])
+
         print('*'*10, query, '*'*10)
-        print(tender_df)
+        print(tender_df_preprocessed)
+        #print(type(tender_df[0]))
+
+        update_results(query, tender_df_preprocessed)

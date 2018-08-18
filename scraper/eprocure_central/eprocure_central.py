5eceaa25039f14c78b1b7611c3d111a2f9516bbf
Nityesh Agarwal <nityeshagarwal@gmail.com>
2018-08-19 00:00:38 +0530

    scraper/: New module containing all the scrapers
    The code responsible for scraping the websites has been moved to this
    module. This module contains various sub-modules, each containing the
    code + the results for scraping a particular website.

---
+++ scraper/eprocure_central/eprocure_central.py
@@ -0,0 +1,24 @@
+import mechanicalsoup as msoup
+import pandas as pd
+
+class EprocureCentral():
+
+    URL = "https://eprocure.gov.in/cppp/tendersearch"
+
+    @classmethod
+    def scrape(cls, query):
+
+        browser = msoup.StatefulBrowser()
+
+        browser.open(cls.URL)
+        browser.select_form()
+        browser.select_form('form[id="tendersearch-form"]')
+        browser["s_keyword"] = query
+        response = browser.submit_selected()
+
+        # get results
+        result_page = browser.get_current_page()
+        tender_table = result_page.find('table', attrs={'id': 'table'})
+        tender_df = pd.read_html(str(tender_table))
+
+        return tender_df[0], cls.URL

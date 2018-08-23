0867f194b2bd7dc2083d8dbeb270f03ec86b5b99
Nityesh Agarwal <nityeshagarwal@gmail.com>
2018-08-23 12:35:18 +0530

    SAIL/: New scraper
    A new scraper class SAIL has been created to help scrape the website -
    "https://sailtenders.co.in/"

---
+++ scraper/SAIL/SAIL.py
@@ -0,0 +1,62 @@
+import time
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class SAIL():
+
+    URL = "https://sailtenders.co.in/"
+
+    @classmethod
+    def preprocess(cls, results_df):
+        '''
+        Preprocesses the given DataFrame object by setting appropriate row
+        and column headers
+
+        :param subject_df: pandas DataFrame object
+        '''
+        results_df.index.names = ['Sl.No.']
+        results_df.index = results_df.index + 1
+
+        return results_df[['Tender issue date and time', 'Tender Title', 'Plant/Unit']]
+
+    @classmethod
+    def check_empty(cls, results_df):
+
+        if list(results_df.iloc[:1]['Tender Title'] == 'No data available in table')[0]:
+            return True
+        else:
+            return False
+
+    @classmethod
+    def scrape(cls, query):
+
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(executable_path='/home/nityeshaga/Downloads/chromedriver', chrome_options=chrome_options)
+
+        driver.get(cls.URL)
+        search_element = driver.find_element_by_id("txtSearchKeyword")
+        search_button = driver.find_element_by_xpath('//*[@id="div_scroll"]/div[2]/div[5]/button')
+        search_element.send_keys(query)
+        search_button.click()
+
+        time.sleep(5)
+        
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        tender_table = soup.find('table', attrs={'id': 'tblTendet'})
+        tender_df = pd.read_html(str(tender_table))
+
+        results_df = cls.preprocess(tender_df[0])
+        results_url = driver.current_url
+
+        driver.quit()
+        
+        if cls.check_empty(results_df):
+            return None, None
+        else:
+            return results_df, results_url
